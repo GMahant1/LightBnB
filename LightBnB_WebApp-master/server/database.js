4d5ae9091cc807b8147432161889fb0bbb82e3f9
Gaurav Mahant <thegau22@gmail.com>
2022-12-15 16:40:27 -0700

    refactor getAllProperties function to use my database instead of json file

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,19 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'gauravmahant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+// the following assumes that you named your connection variable `pool`
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+
+
 /// Users
 
 /**
@@ -67,12 +80,18 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+  //console.log(limit);
+  const queryString = `SELECT * FROM properties LIMIT $1;`;
+  return pool
+    .query(queryString, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.getAllProperties = getAllProperties;
 
 
