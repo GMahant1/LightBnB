53bf262f1214410254391746668a2f9f78885caf
Gaurav Mahant <thegau22@gmail.com>
2022-12-17 20:31:08 -0700

    refactor get reservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -26,7 +26,7 @@ const getUserWithEmail = (email) => {
   return pool
     .query(queryString, [email])
     .then((result) => {
-      console.log(result);
+      //console.log(result);
       if (result.rows === []) {
         return null;
       }
@@ -84,8 +84,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  const queryString = `SELECT reservations.*, properties.* FROM reservations JOIN properties ON properties.id = reservations.property_id WHERE guest_id = $1 LIMIT $2;`;
+  return pool
+    .query(queryString, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
